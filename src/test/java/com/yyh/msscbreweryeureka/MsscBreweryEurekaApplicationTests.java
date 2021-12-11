6c56f0c4a07e02683041e3b00ef943f5d0480f45
yyhyap <yongheng98@hotmail.com>
2021-12-11 09:44:13 +0800

    Initial commit

---
+++ src/test/java/com/yyh/msscbreweryeureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyh.msscbreweryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
